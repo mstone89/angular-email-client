c940a1c9e3d85c51df81d9ee33a6e9a0ad587f91
Molly Stone <molly.stone89@gmail.com>
2020-02-27 20:12:45 -0500

    Basic routing setup

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    { path: 'signup', component: SignupComponent },
+    { path: '', component: SigninComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class AuthRoutingModule { }
