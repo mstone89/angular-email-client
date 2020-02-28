355331e353dc210c242d4202c2b4ed950708b313
Molly Stone <molly.stone89@gmail.com>
2020-02-28 18:15:58 -0500

    Create unique username validator

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
@@ -12,7 +13,8 @@ import { SignupComponent } from './signup/signup.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatchPassword } from './../validators/match-password';
+import { UniqueUsername } from './../validators/unique-username';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
     authForm = new FormGroup({
@@ -14,7 +15,7 @@ export class SignupComponent implements OnInit {
             Validators.minLength(3),
             Validators.maxLength(20),
             Validators.pattern(/^[a-z0-9]+$/)
-        ]),
+        ], [this.uniqueUsername.validate]),
         password: new FormControl('', [
             Validators.required,
             Validators.minLength(4),
@@ -27,9 +28,12 @@ export class SignupComponent implements OnInit {
         ])
     }, { validators: [this.matchPassword.validate] });
 
-  constructor(private matchPassword: MatchPassword) { }
+    constructor(
+        private matchPassword: MatchPassword,
+        private uniqueUsername: UniqueUsername
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
