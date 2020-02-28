1af7af99b930adadf6c7435db4c3498b5ca473c5
Molly Stone <molly.stone89@gmail.com>
2020-02-27 20:18:41 -0500

    Import reactive form module, create sign up form group

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
@@ -10,7 +11,8 @@ import { SignupComponent } from './signup/signup.component';
   declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
