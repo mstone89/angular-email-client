eccde2ffddc76b424c3ba260928885b35c1d83d8
Molly Stone <molly.stone89@gmail.com>
2020-03-07 20:23:05 -0500

    Redirect user in guard if not signed in

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,19 +1,27 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { skipWhile, take } from 'rxjs/operators';
+import { skipWhile, take, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-    constructor(private auth: AuthService) {}
+    constructor(
+        private auth: AuthService,
+        private router: Router
+    ) {}
 
     canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
         return this.auth.signedin$.pipe(
-            skipWhile(status => status === null),
-            take(1)
+            skipWhile(value => value === null),
+            take(1),
+            tap((authenticated) => {
+                if (!authenticated) {
+                    this.router.navigateByUrl('/');
+                }
+            })
         );
     }
 }
