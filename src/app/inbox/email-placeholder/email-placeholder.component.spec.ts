d45b3af6b7b05f9d214e6330dcacab31baf6239e
Molly Stone <molly.stone89@gmail.com>
2020-03-07 21:13:28 -0500

    Add router link for children component

---
+++ src/app/inbox/email-placeholder/email-placeholder.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmailPlaceholderComponent } from './email-placeholder.component';
+
+describe('EmailPlaceholderComponent', () => {
+  let component: EmailPlaceholderComponent;
+  let fixture: ComponentFixture<EmailPlaceholderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EmailPlaceholderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmailPlaceholderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,10 +1,19 @@
+import { EmailShowComponent } from './email-show/email-show.component';
+import { EmailPlaceholderComponent } from './email-placeholder/email-placeholder.component';
 import { InboxHomeComponent } from './inbox-home/inbox-home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{
-    path: '', component: InboxHomeComponent
-}];
+const routes: Routes = [
+    {
+        path: '',
+        component: InboxHomeComponent,
+        children: [
+            { path: ':id', component: EmailShowComponent },
+            { path: '', component: EmailPlaceholderComponent }
+        ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
