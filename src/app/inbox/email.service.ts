6a0bc9374467762a684a6d85ad92806462d232e8
Molly Stone <molly.stone89@gmail.com>
2020-03-07 20:59:24 -0500

    Create email service, get emails, render list of emails

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface EmailSummary {
+    id: string;
+    subject: string;
+    from: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EmailService {
+    rootUrl = 'https://api.angular-email.com';
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
+    getEmails() {
+        const url = `${this.rootUrl}/emails`;
+        return this.http.get<EmailSummary[]>(url);
+    }
+
+    getEmailById(id: string) {
+        const url = `${this.rootUrl}/emails/${id}`;
+        return this.http.get(url);
+    }
+
+    sendEmail() {
+
+    }
+
 }
