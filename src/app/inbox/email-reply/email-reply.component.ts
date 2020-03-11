44e7369fcfa57b7805b8c987ed7eb8e0669296b9
Molly Stone <molly.stone89@gmail.com>
2020-03-10 21:55:56 -0400

    Reply to emails, and formatting touches

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { EmailService } from './../email.service';
+import { Email } from './../email';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
-  selector: 'app-email-reply',
-  templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+    selector: 'app-email-reply',
+    templateUrl: './email-reply.component.html',
+    styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent implements OnChanges {
+    showModal = false;
+    @Input() email: Email;
 
-  constructor() { }
+    constructor(private emailService: EmailService) { }
 
-  ngOnInit() {
-  }
+    ngOnChanges() {
+        const replaceNewlines = this.email.text.replace(/\n/gi, '\n> ');
+        const finalText = `\n\n\n-------- ${this.email.from} wrote:\n> ${replaceNewlines}`;
 
+        this.email = {
+            ...this.email,
+            from: this.email.to,
+            to: this.email.from,
+            subject: `RE:${this.email.subject}`,
+            text: finalText,
+            html: finalText
+        };
+    }
+
+    onSubmit(email: Email) {
+        this.emailService.sendEmail(email).subscribe(() => {
+            this.showModal = false;
+        });
+    }
 }
